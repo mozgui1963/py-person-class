d306723bc6001c27b29f385cd7514690d8438f4b
Olivier Mbayi <olimbayi@gmail.com>
2025-05-23 23:45:58 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -6,7 +6,6 @@ class Person:
         self.age = age
         self.wife = None
         self.husband = None
-        Person.people[name] = self
 
     def __repr__(self):
         return f"Person(name={self.name}, age={self.age})"
@@ -17,16 +16,15 @@ def create_person_list(people_data: list) -> list:
     people_instances = []
 
     for person in people_data:
-        instance = Person(person["name"], person["age"])
+        instance = person["name"], person["age"]
         people_instances.append(instance)
 
     for person in people_data:
-        instance = Person.people[person["name"]]
+        instance = person["name"]
         spouse_key = "wife" if "wife" in person else "husband"
         spouse_name = person.get(spouse_key)
 
         if spouse_name:
-            setattr(instance, spouse_key, Person.people.get(spouse_name))
+            setattr(instance, spouse_key, person.get(spouse_name))
 
     return people_instances
-
